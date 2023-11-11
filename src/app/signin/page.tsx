7e0225dee4888c3f0197bb0dd6e2c8c3f3ca9bf1
Hoang Hy <hoanghy0112003@gmail.com>
2023-11-11 15:44:58 +0700

    config: add new color

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+
+export default function Page() {}
+
+export const metadata: Metadata = {
+	title: "Sign in to ESMS",
+	description: "Electronic Store Management System",
+};
