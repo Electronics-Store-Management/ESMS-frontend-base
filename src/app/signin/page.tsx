e8449b8c06397f7726532d5dd959528e41dadd8d
Hoang Hy <hoanghy0112003@gmail.com>
2023-11-11 19:08:35 +0700

    dev: add sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,20 +1,29 @@
+"use client";
+
 import Button from "@/components/Button/Button";
-import { Metadata } from "next";
+import TextInput from "@/components/Input/TextInput";
+import { HiMail } from "react-icons/hi";
 
 export default function Page() {
 	return (
-		<>
-			<Button type="error" fill={false}>
-				Sign in
-			</Button>
-			<Button type="secondary" fill={false}>
-				Sign in
-			</Button>
-		</>
+		<div className=" h-screen grid grid-cols-3">
+			<div className=" col-span-2"></div>
+			<div className=" col-span-1">
+				<div>
+					<h1>Sign in to our platform</h1>
+					<TextInput
+						className="mt-8"
+						title="Email"
+						icon={HiMail}
+						placeholder="yourmail@gmail.com"
+					/>
+					<TextInput
+						className=" mt-5"
+						title="Password"
+						placeholder="Enter your password"
+					/>
+				</div>
+			</div>
+		</div>
 	);
 }
-
-export const metadata: Metadata = {
-	title: "Sign in to ESMS",
-	description: "Electronic Store Management System",
-};
