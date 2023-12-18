21a84d4d03f53384f1224bdb5b8d9b6b0dd79ef2
Thanh Loc <thanhlocne246@gmail.com>
2023-12-18 07:41:33 +0700

    remove createdDate,... from BaseEntity to prepare for transfering to enver in backend

--- src/types/entity/BaseEntity.ts
+++ src/types/entity/BaseEntity.ts
@@ -2,8 +2,8 @@ import StaffTouch from "./StaffTouch";
 
 export default interface BaseEntity {
     id: string;
-    createdDate: Date;
-    modifiedDate: Date;
-    createdBy: StaffTouch;
-    modifiedBy: StaffTouch;
+    createdDate?: Date;
+    modifiedDate?: Date;
+    createdBy?: StaffTouch;
+    modifiedBy?: StaffTouch;
 }
