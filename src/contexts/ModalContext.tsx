283817b3def721c57aa4edd5a6bace05ec14f5ac
Thanh Loc <thanhlocne246@gmail.com>
2023-12-18 07:41:33 +0700

    fix authentication

--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -3,19 +3,14 @@
 import ClaimModal from "@/components/ClaimModal/ClaimModal";
 import CreateCategoryFormModal from "@/components/CreateCategoryForm/CreateCategoryFormModal";
 import CreateProductFormModal from "@/components/CreateProductForm/CreateProductFormModal";
+import CreateStaffFormModal from "@/components/CreateStaffForm/CreateStaffFormModal";
 import UpdateCategoryFormModal from "@/components/UpdateCategoryForm/UpdateCategoryFormModal";
 import UpdateProductFormModal from "@/components/UpdateProductForm/UpdateProductFormModal";
 import { ReactNodeChildren } from "@/types/ReactNodeChildren";
 import { ReactNode, createContext, useState } from "react";
 
 export function ModalProvider({ children }: ReactNodeChildren) {
-    const [modalState, setModalState] = useState<IModalState>({
-        addProduct: { isOpen: false },
-        updateProduct: { isOpen: false },
-        addCategory: { isOpen: false },
-        updateCategory: { isOpen: false },
-        claim: { isOpen: false },
-    });
+    const [modalState, setModalState] = useState<IModalState>(defaultValue);
 
     return (
         <ModalStateContext.Provider
@@ -31,19 +26,23 @@ export function ModalProvider({ children }: ReactNodeChildren) {
             <UpdateProductFormModal />
             <CreateCategoryFormModal />
             <UpdateCategoryFormModal />
+            <CreateStaffFormModal />
             <ClaimModal />
         </ModalStateContext.Provider>
     );
 }
 
+const defaultValue = {
+    addProduct: { isOpen: false },
+    addStaff: { isOpen: false },
+    updateProduct: { isOpen: false },
+    addCategory: { isOpen: false },
+    updateCategory: { isOpen: false },
+    claim: { isOpen: false },
+};
+
 export const ModalStateContext = createContext<IModalStateContext>({
-    modalState: {
-        addProduct: { isOpen: false },
-        updateProduct: { isOpen: false },
-        addCategory: { isOpen: false },
-        updateCategory: { isOpen: false },
-        claim: { isOpen: false },
-    },
+    modalState: defaultValue,
     setModalState: () => {},
 });
 
@@ -56,6 +55,7 @@ export type IModalStateContext = {
 
 export type IModalState = {
     addProduct: IModalStateItem;
+    addStaff: IModalStateItem;
     updateProduct: IModalStateItem & { productId?: string };
     addCategory: IModalStateItem;
     updateCategory: IModalStateItem & { categoryId?: string };
@@ -69,4 +69,3 @@ export type IModalStateItem = {
     isOpen: boolean;
     refetch?: () => any;
 };
-
